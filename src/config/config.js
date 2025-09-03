7975293eb2203ee412450b72f4ab9e20a5e06a95
hungnv2 <dntvh137@gmail.com>
2025-09-03 10:23:06 +0700

    fix

---
+++ src/config/config.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER || 'root',
+    password: process.env.DB_PASS || '123456',
+    database: process.env.DB_NAME || 'shopyody_new',
+    host: process.env.DB_HOST || 'localhost',
+    dialect: 'mysql'
+  },
+  test: {
+    username: process.env.DB_USER || 'root',
+    password: process.env.DB_PASS || '123456',
+    database: 'cloth_test',
+    host: process.env.DB_HOST || 'localhost',
+    dialect: 'mysql'
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    logging: false
+  }
+};
